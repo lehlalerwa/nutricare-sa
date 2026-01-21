e50cd8145175ab267ac204861b6a17ce82506349
john mojapelo <135161510+lehlalerwa@users.noreply.github.com>
2026-01-21 15:12:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,34 @@
-const CACHE_NAME = "nutricare-sa-v1";
+const CACHE_NAME = "nutricare-sa-v2";
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  // CSV files
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQG1Q3SV22JE3A54kFrIrNIXA4W1ah8zxSNFn-nR3OsNIevSG6ybpIJV_d2_zlbP1FwzhCyCs2JN8x0/pub?gid=1989255355&single=true&output=csv",
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR8jGwgvVsgAefxBFcXEhWsqNbnU6trUosFe-FLvr-E_IvZH44txGhH8g5FQDcDy2NbgRqAx_6eJdnO/pub?gid=2024151986&single=true&output=csv",
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTysJgBs-JnGgjMqTE8ifEixLZIwl38ykLjB6tnk4aXUhtw0EqoAxh5iRPhqEuIIuWXbrfRqyfaKgBx/pub?gid=0&single=true&output=csv"
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
   );
   self.skipWaiting();
 });
 
-// ACTIVATE
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// FETCH (online first, offline fallback)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, clone);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
